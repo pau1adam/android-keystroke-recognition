9a90005defff79756cf53760f47aab7b89a25bea
Pavlos Adam <pavlosadam@gmail.com>
2017-07-29 00:05:56 +0300

    Migrated main activity to kotlin.

---
+++ app/src/main/java/padam/keystroke_recognition/MainActivity.kt
@@ -0,0 +1,26 @@
+package padam.keystroke_recognition
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        goToPressureTestButton.setOnClickListener { _ -> goToPressureTestActivity() }
+        goToEditTextKeyboardTestButton.setOnClickListener { _ -> goToEditTextKeyboardTestActivity() }
+    }
+
+    fun goToPressureTestActivity() {
+        val pressureTestIntent = Intent(this, PressureTestActivity::class.java)
+        startActivity(pressureTestIntent)
+    }
+
+    fun goToEditTextKeyboardTestActivity() {
+        val editTextKeyboadTestIntent = Intent(this, EditTextKeyboardTestActivity::class.java)
+        startActivity(editTextKeyboadTestIntent)
+    }
+}
