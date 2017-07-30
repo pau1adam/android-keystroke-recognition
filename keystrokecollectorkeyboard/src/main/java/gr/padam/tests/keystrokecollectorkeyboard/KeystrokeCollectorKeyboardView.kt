d52637025389995492f792b4d7193176210168a1
Pavlos Adam <pavlosadam@gmail.com>
2017-07-30 23:20:49 +0300

    Added KeystrokeCollectorKeyboard module with KeystrokeCollectorKeyboardView.

---
+++ keystrokecollectorkeyboard/src/main/java/gr/padam/tests/keystrokecollectorkeyboard/KeystrokeCollectorKeyboardView.kt
@@ -0,0 +1,16 @@
+package gr.padam.tests.keystrokecollectorkeyboard
+
+import android.content.Context
+import android.inputmethodservice.KeyboardView
+import android.util.AttributeSet
+
+/**
+ * Created by Pavlos Adam on 30-Jul-17.
+ */
+class KeystrokeCollectorKeyboardView(context: Context, attrs: AttributeSet) : KeyboardView(context, attrs) {
+
+    init {
+
+    }
+
+}
