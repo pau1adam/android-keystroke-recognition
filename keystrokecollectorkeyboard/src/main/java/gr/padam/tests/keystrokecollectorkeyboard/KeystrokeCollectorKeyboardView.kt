09b57d964819da9e60794582748cd2c2930fcaae
Pavlos Adam <pavlosadam@gmail.com>
2017-08-10 21:37:47 +0300

    Added placeholder listeners.

--- keystrokecollectorkeyboard/src/main/java/gr/padam/tests/keystrokecollectorkeyboard/KeystrokeCollectorKeyboardView.kt
+++ keystrokecollectorkeyboard/src/main/java/gr/padam/tests/keystrokecollectorkeyboard/KeystrokeCollectorKeyboardView.kt
@@ -3,14 +3,55 @@ package gr.padam.tests.keystrokecollectorkeyboard
 import android.content.Context
 import android.inputmethodservice.KeyboardView
 import android.util.AttributeSet
+import android.view.View
 
 /**
  * Created by Pavlos Adam on 30-Jul-17.
  */
 class KeystrokeCollectorKeyboardView(context: Context, attrs: AttributeSet) : KeyboardView(context, attrs) {
 
-    init {
+    val touchListener = View.OnTouchListener { view, event ->
+        TODO("not implemented")
+    }
 
+    val keyboardListener = object : OnKeyboardActionListener {
+
+        override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+            TODO("not implemented")
+        }
+
+        override fun swipeRight() {
+            TODO("not implemented")
+        }
+
+        override fun onPress(p0: Int) {
+            TODO("not implemented")
+        }
+
+        override fun onRelease(primaryCode: Int) {
+            TODO("not implemented")
+        }
+
+        override fun swipeLeft() {
+            TODO("not implemented")
+        }
+
+        override fun swipeUp() {
+            TODO("not implemented")
+        }
+
+        override fun swipeDown() {
+            TODO("not implemented")
+        }
+
+        override fun onText(text: CharSequence?) {
+            TODO("not implemented")
+        }
+    }
+
+    init {
+        setOnTouchListener(touchListener)
+        onKeyboardActionListener = keyboardListener
     }
 
 }
