923e244db7ee735288bfec69295db998494c887d
Pavlos Adam <pavlosadam@gmail.com>
2017-08-22 00:07:11 +0300

    Show keyboard view on EditTextKeyboardTestActivity.

--- app/src/main/java/padam/keystroke_recognition/EditTextKeyboardTestActivity.kt
+++ app/src/main/java/padam/keystroke_recognition/EditTextKeyboardTestActivity.kt
@@ -1,5 +1,6 @@
 package padam.keystroke_recognition
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_edit_text_keyboad_test.*
@@ -10,9 +11,11 @@ class EditTextKeyboardTestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_text_keyboad_test)
         resetButton.setOnClickListener { _ -> resetFields() }
+        var keyboard = Keyboard(this, R.xml.qwerty)
+        keyboardView.keyboard = keyboard
     }
 
-    fun resetFields() {
+    private fun resetFields() {
         keyEventData.text = ""
         motionEventData.text = ""
         inputEditText.setText("")
