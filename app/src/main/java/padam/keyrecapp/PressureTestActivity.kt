2d747fa634d88b80eef0f177cb0a0f2e7354cd0a
Pavlos Adam <pavlosadam@gmail.com>
2017-12-02 16:18:57 +0200

    Implemented UI state saving with ViewModel on PressureTestActivity.

--- app/src/main/java/padam/keyrecapp/PressureTestActivity.kt
+++ app/src/main/java/padam/keyrecapp/PressureTestActivity.kt
@@ -1,46 +1,38 @@
 package padam.keyrecapp
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_pressure_test.*
 
 class PressureTestActivity : AppCompatActivity() {
 
-    private var maxPressure = -1f
-    private var maxSize = -1f
+    private lateinit var model: PressureTestViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pressure_test)
-        activity_pressure_test.setOnTouchListener { _, event -> pressureTestClick(event) }
-        resetButton.setOnClickListener { resetFields() }
+        model = ViewModelProviders.of(this).get(PressureTestViewModel::class.java)
+        setupUI()
     }
 
-    private fun pressureTestClick(event: MotionEvent): Boolean {
-        displayPressure.text = "Pressure: " + event.pressure
-        displaySize.text = "Size: " + event.size
-        maxValues(event.pressure, event.size)
-        return true
-    }
+    private fun setupUI() {
+        activity_pressure_test.setOnTouchListener { _, event -> model.pressureTestClick(event) }
+        resetButton.setOnClickListener { model.reset() }
 
-    private fun resetFields() {
-        displayPressure.text = ""
-        displaySize.text = ""
-        displayMaxPressure.text = ""
-        displayMaxSize.text = ""
-        maxPressure = -1f
-        maxSize = -1f
-    }
+        model.maxPressureText.observe(this, Observer {
+            displayMaxPressure.text = model.maxPressureText.value
+        })
+        model.pressureText.observe(this, Observer {
+            displayPressure.text = model.pressureText.value
+        })
 
-    private fun maxValues(pressure: Float, size: Float) {
-        if (maxPressure < pressure) {
-            maxPressure = pressure
-            displayMaxPressure.text = "Maximum Pressure: " + maxPressure.toString()
-        }
-        if (maxSize < size) {
-            maxSize = size
-            displayMaxSize.text = "Maximum Size: " + maxSize.toString()
-        }
+        model.maxSizeText.observe(this, Observer {
+            displayMaxSize.text = model.maxSizeText.value
+        })
+        model.sizeText.observe(this, Observer {
+            displaySize.text = model.sizeText.value
+        })
     }
 }
