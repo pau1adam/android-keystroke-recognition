b1f249a7f1ab58cc1c34c938f2a9fe15293b8ca1
Pavlos Adam <pavlosadam@gmail.com>
2018-05-02 23:25:37 +0300

    Removed unused import.

--- keystrokecollectorkeyboard/src/main/java/gr/padam/tests/keystrokecollectorkeyboard/KeystrokeCollectorKeyboardView.kt
+++ keystrokecollectorkeyboard/src/main/java/gr/padam/tests/keystrokecollectorkeyboard/KeystrokeCollectorKeyboardView.kt
@@ -6,7 +6,6 @@ import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
 import android.text.Editable
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.EditText
