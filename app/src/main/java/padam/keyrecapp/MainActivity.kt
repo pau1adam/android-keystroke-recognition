deda20f8319fe3c7fab6db5e89b9eeb54679027d
Pavlos Adam <pavlosadam@gmail.com>
2018-05-03 21:18:11 +0300

    Added missing toolbar to main activity after theme change.

--- app/src/main/java/padam/keyrecapp/MainActivity.kt
+++ app/src/main/java/padam/keyrecapp/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         goToPressureTestButton.setOnClickListener { goToPressureTestActivity() }
         goToEditTextKeyboardTestButton.setOnClickListener { goToEditTextKeyboardTestActivity() }
     }
